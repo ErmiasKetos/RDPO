ec56f67917489dcbca34d795cb230557c64a9765
ErmiasKetos <123589824+ErmiasKetos@users.noreply.github.com>
2024-12-02 16:58:31 -0800

    Update google_auth.py

--- google_auth.py
+++ google_auth.py
@@ -1,15 +1,18 @@
 import streamlit as st
 from google.oauth2.credentials import Credentials
+from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
 SCOPES = ['https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/gmail.send']
 
 def get_google_creds():
     creds = None
-    if 'google_token' in st.secrets:
-        creds = Credentials.from_authorized_user_info(st.secrets['google_token'], SCOPES)
+    if 'google_client_secret' in st.secrets:
+        client_config = st.secrets['google_client_secret']
+        creds = service_account.Credentials.from_service_account_info(
+            client_config, scopes=SCOPES)
     if not creds or not creds.valid:
-        st.error("Google credentials are not valid. Please set up the Google token in Streamlit secrets.")
+        st.error("Google credentials are not valid. Please set up the Google client secret in Streamlit secrets.")
         st.stop()
     return creds
 
@@ -20,4 +23,6 @@ def get_drive_service():
 def get_gmail_service():
     creds = get_google_creds()
     return build('gmail', 'v1', credentials=creds)
+
+
 
