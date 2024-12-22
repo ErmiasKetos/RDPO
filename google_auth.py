b615b344d225c54f2719f6eb7e4241a39c9dd88e
Ermias <123589824+ErmiasKetos@users.noreply.github.com>
2024-12-21 23:06:15 -0800

    Update google_auth.py

--- google_auth.py
+++ google_auth.py
@@ -9,6 +9,7 @@ def get_credentials():
             st.secrets["gcp_service_account"],
             scopes=[
                 "https://www.googleapis.com/auth/spreadsheets",
+                "https://www.googleapis.com/auth/drive",  # Full Drive access
                 "https://www.googleapis.com/auth/drive.file",
                 "https://www.googleapis.com/auth/gmail.send",
                 "https://mail.google.com/"
